69ddddf18e6659ee5d1d6784b95e612e305aa958
William <williampv@gmail.com>
2025-07-23 11:59:19 -0500

    navbar, table and form added

---
+++ src/views/EmployeesView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+
+        <form>
+            <div class="form-group">
+                <label for="name">Enter Name</label>
+                <input type="text" name="name" class="form-control" >
+            </div>
+
+            <div class="form-group">
+                <label for="name">Enter Email</label>
+                <input type="text" name="email" class="form-control" >
+            </div>
+
+            <button class="btn btn-primary btn-sm mt-4">Submit</button>
+        </form>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- <tr *ngFor="let emp of employees">
+                    <td>{{ emp.id }}</td>
+                    <td>{{ emp.name }}</td>
+                    <td>{{ emp.email }}</td>
+                </tr> -->
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
